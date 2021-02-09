ac24f2a6b8d638645999850ebdefc5eb7f600c55
Nicholas Evans <nicholasaevans@gmail.com>
2021-02-08 22:01:12 -0600

    Fix classname

--- src/pages/case-studies.js
+++ src/pages/case-studies.js
@@ -53,7 +53,7 @@ const CaseStudies = () => (
             </p>
             <a href={IMDAPIPDF} target="_blank" rel="noreferrer noopener" className="inline-flex items-center">
               Download PDF of case study
-              <svg class="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h14"></path><path d="M12 5l7 7-7 7"></path></svg>
+              <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h14"></path><path d="M12 5l7 7-7 7"></path></svg>
             </a>
           </div>
         </div>

efc91bf96b5361f87eb7c37188bdb3a4699218dd
Trevor Ewen <trevor@stg.software>
2021-08-05 06:03:20 -0400

    Sanford Law Firm case study.

--- src/pages/case-studies.js
+++ src/pages/case-studies.js
@@ -5,9 +5,11 @@ import SEO from "../components/seo"
 import FooterCTA from "../components/footer-call-to-action"
 
 import PRco from "../images/prco-car.webp"
+import SanfordLawFirmCourthouse from "../images/sanford-law-firm-courthouse.webp"
 import VerdeLighting from "../images/verde-lighting.webp"
 import IMDApi from "../images/imd-api.webp"
 import PRcoPDF from "../../static/PRco Case Study_Southport Technology Group.pdf"
+import SanfordLawFirmPDF from "../../static/Sanford Law Firm Case Study_Southport Technology Group.pdf"
 import VerdePDF from "../../static/Verde Case Study_Southport Technology Group.pdf"
 import IMDAPIPDF from "../../static/IMD API Technical Due Diligence_Southport Technology Group.pdf"
 
@@ -28,6 +30,19 @@ const CaseStudies = () => (
               Transforming an automobile service contract administrator through automation.
             </p>
             <a href={PRcoPDF} target="_blank" rel="noreferrer noopener" className="inline-flex items-center border-b border-blue-700 border-opacity-0 lg:hover:border-opacity-100 transition-all">
+              View PDF of case study
+              <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h14"></path><path d="M12 5l7 7-7 7"></path></svg>
+            </a>
+          </div>
+        </div>
+        <div className="bg-white border border-blue-200 shadow-xl mb-10">
+          <img className="w-100" src={SanfordLawFirmCourthouse} alt="Courthouse" />
+          <div className="p-5 md:p-10">
+            <h4 className="text-xl mb-2">Sanford Law Firm</h4>
+            <p className="lg:text-2xl mb-10">
+              From the federal courts to internal operations. Backgrounding document retrieval & management.
+            </p>
+            <a href={SanfordLawFirmPDF} target="_blank" rel="noreferrer noopener" className="inline-flex items-center border-b border-blue-700 border-opacity-0 lg:hover:border-opacity-100 transition-all">
               View PDF of case study
               <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h14"></path><path d="M12 5l7 7-7 7"></path></svg>
             </a>

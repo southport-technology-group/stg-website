f89d7caefcb7c2607b7a92711846e15beef53c7c
Trevor Ewen <trevor@stg.software>
2021-08-16 13:14:40 -0500

    Haven Equity case study. (#17)

--- src/pages/case-studies.js
+++ src/pages/case-studies.js
@@ -6,10 +6,12 @@ import FooterCTA from "../components/footer-call-to-action"
 
 import PRco from "../images/prco-car.webp"
 import SanfordLawFirmCourthouse from "../images/sanford-law-firm-courthouse.webp"
+import HavenEquityTable from '../images/haven-equity-table.webp';
 import VerdeLighting from "../images/verde-lighting.webp"
 import IMDApi from "../images/imd-api.webp"
 import PRcoPDF from "../../static/PRco Case Study_Southport Technology Group.pdf"
 import SanfordLawFirmPDF from "../../static/Sanford Law Firm Case Study_Southport Technology Group.pdf"
+import HavenEquityPDF from "../../static/Haven Equity Case Study_Southport Technology Group.pdf"
 import VerdePDF from "../../static/Verde Case Study_Southport Technology Group.pdf"
 import IMDAPIPDF from "../../static/IMD API Technical Due Diligence_Southport Technology Group.pdf"
 
@@ -49,11 +51,24 @@ const CaseStudies = () => (
           </div>
         </div>
         <div className="bg-white border border-blue-200 shadow-xl mb-10">
+          <img className="w-100" src={HavenEquityTable} alt="Courthouse" />
+          <div className="p-5 md:p-10">
+            <h4 className="text-xl mb-2">Haven Equity</h4>
+            <p className="lg:text-2xl mb-10">
+              Streamlining time entry and accounting practices for an Australian holding company.
+            </p>
+            <a href={HavenEquityPDF} target="_blank" rel="noreferrer noopener" className="inline-flex items-center border-b border-blue-700 border-opacity-0 lg:hover:border-opacity-100 transition-all">
+              View PDF of case study
+              <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round"><path d="M5 12h14"></path><path d="M12 5l7 7-7 7"></path></svg>
+            </a>
+          </div>
+        </div>
+        <div className="bg-white border border-blue-200 shadow-xl mb-10">
           <img className="w-100" src={VerdeLighting} alt="Lights in restaurant" />
           <div className="p-5 md:p-10">
             <h4 className="text-xl mb-2">Verde Energy Efficiency Experts</h4>
             <p className="lg:text-2xl mb-10">
-              Streamlining and automating the sales process for a LED retrofitting and energy efficiency business.
+              Automating the sales process for a LED retrofitting and energy efficiency business.
             </p>
             <a href={VerdePDF} target="_blank" rel="noreferrer noopener" className="inline-flex items-center border-b border-blue-700 border-opacity-0 lg:hover:border-opacity-100 transition-all">
               View PDF of case study
